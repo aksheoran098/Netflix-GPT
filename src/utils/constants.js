50294ffa2872fcc76de553b3f3dee99c647ada58
aksheoran098 <60999606+aksheoran098@users.noreply.github.com>
2026-07-01 23:32:06 +0530

    Update constants.js
    Updated backgrounf image url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const USER_AVATAR =
   "https://cdn-icons-png.flaticon.com/512/3177/3177440.png";
 
 export const BACKGROUND_IMAGE =
-  "https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_large.jpg";
+  "https://assets.nflxext.com/ffe/siteui/vlv3/1d29f9a4-1900-43dc-a420-99044f734ee2/cc3b7bcb-3f79-449e-a37c-26ffb20fce3c/IN-en-20240826-POP_SIGNUP_TWO_WEEKS-perspective_WEB_7a193436-88c7-4f66-a8f0-e191d3b26d13_large.jpg";
 
 export const API_OPTIONS = {
   method: "GET",
